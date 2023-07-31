7d5745c96535ffda38bb4ed2c10ffd044935724d
leroysb <leroybuliro@gmail.com>
2023-07-31 23:03:51 +0300

    parameterize and patch

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -33,5 +33,52 @@ class TestAccessNestedMap(unittest.TestCase):
             access_nested_map(nested_map, path)
 
 
+class TestGetJson(unittest.TestCase):
+    """TestGetJson class
+    """
+
+    @parameterized.expand([
+        ("http://example.com", {"payload": True}),
+        ("http://holberton.io", {"payload": False})
+    ])
+    def test_get_json(self, test_url, test_payload):
+        """test_get_json function.
+        Checks that utils.get_json returns the expected result."""
+        mock = Mock()
+        mock.json.return_value = test_payload
+        with patch('requests.get', return_value=mock):
+            self.assertEqual(get_json(test_url), test_payload)
+            mock.json.assert_called_once()
+
+
+class TestMemoize(unittest.TestCase):
+    """TestMemoize class
+    """
+
+    def test_memoize(self):
+        """test_memoize function.
+        Tests that when calling a_property twice, the correct result is
+        returned but a_method is only called once using assert_called_once
+        """
+
+        class TestClass:
+            def a_method(self):
+                """a_method function.
+                """
+                return 42
+
+            @memoize
+            def a_property(self):
+                """a_property function.
+                """
+                return self.a_method()
+
+        with patch.object(TestClass, 'a_method') as mock:
+            test_class = TestClass()
+            test_class.a_property()
+            test_class.a_property()
+            mock.assert_called_once()
+
+
 if __name__ == "__main__":
     unittest.main()
